f932fc0d9efe321026f234cf12c89d8fc2a9d0a6
Dupeljan Linux <dupeljan@gmail.com>
2019-03-15 07:01:38 +0300

    Try to filter certan size sticks

--- main.cpp
+++ main.cpp
@@ -11,7 +11,9 @@
 
 using namespace cv;
 using namespace std;
-
+void my_inv(Mat in){
+    threshold(in,in,127,255,THRESH_BINARY_INV);
+}
 int main(int argc, char *argv[])
 {
     string imageName(PIC);
@@ -30,6 +32,10 @@ int main(int argc, char *argv[])
 
     cvtColor(gaus_im, src_gray, COLOR_BGR2GRAY);
 
+    threshold(src_gray,src_gray,180,255,THRESH_BINARY );
+
+
+    // Sobel
     Mat grad_x, grad_y;
     Mat abs_grad_x, abs_grad_y;
     int ddepth = CV_8U;
@@ -39,12 +45,61 @@ int main(int argc, char *argv[])
     // converting back to CV_8U
     convertScaleAbs(grad_x, abs_grad_x);
     convertScaleAbs(grad_y, abs_grad_y);
+    // end Sobel
+
+    // Canny
+    Mat canny_out;
+    Canny(src_gray,canny_out,3,3);
+    //OutputArrayOfArrays counturs;
+    //findContours(src_gray,counturs,RetrievalModes,ContourApproximationModes);
 
     addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0, grad);
 
-    namedWindow( "Display window", WINDOW_AUTOSIZE ); // Create a window for display.
-    imshow( "Display window", grad );                // Show our image inside it.
+    Mat morphology_out;
+    morphologyEx(grad,morphology_out,MORPH_CLOSE, getStructuringElement( MORPH_RECT,Size(3,3) ) );
 
+    Mat inv;
+    threshold(morphology_out,inv,127,255,THRESH_BINARY_INV);
+    // end Canny
+
+    // My filter
+    float left_filter[9] = {1,  0,   -1,
+                           2,   0,   -2,
+                           1,   0,   -1};
+    float bottom_filter[9] {-1, -2, -1,
+                             0,  0,  0,
+                             1,  2,  1};
+    Mat left_border, bottom_border;
+    filter2D(src_gray,left_border,-1, Mat(3,3,CV_32F,left_filter) );
+    filter2D(src_gray,bottom_border,-1, Mat(3,3,CV_32F,bottom_filter));
+    Mat my_filtered;
+    addWeighted(left_border,0.5,bottom_border,0.5,0,my_filtered);
+    // end filter
+    my_inv(my_filtered);
+    // Start morphology
+    Mat mask;
+    //erode(left_border,result,getStructuringElement(MORPH_RECT,Size(1,20)));
+    morphologyEx(left_border,left_border,MORPH_OPEN,getStructuringElement(MORPH_RECT,Size(1,10)));// нижняя граница
+    morphologyEx(left_border,mask,MORPH_OPEN,getStructuringElement(MORPH_RECT,Size(1,15)));// верхняя граница
+    my_inv(left_border);//инвертируем
+    Mat result;
+    addWeighted(left_border,1,mask,1,0,result);//Соединияем
+
+    //my_inv(left_border);
+    //my_inv(result);
+    erode(result,result,getStructuringElement(MORPH_RECT,Size(11,5)));//добавим
+    // end
+    /*
+    int n = 10;
+    Mat vect[n];
+    namedWindow( "Display window", WINDOW_AUTOSIZE ); // Create a window for display.
+    for ( int i = 1; i < n; i++){
+         erode(my_filtered,vect[i],getStructuringElement(MORPH_RECT,Size(i*10,5)));
+         imshow(to_string(i*10),vect[i]);
+    }
+    */
+    imshow( "left_border", left_border);                // Show our image inside it.
+    imshow("result",result);
     waitKey(0); // Wait for a keystroke in the window
     return 0;
 }
